3f88ded81300b55aa49409d8df8dab76f6d24ead
Kyle Harrison <khwebmail@gmail.com>
2019-01-26 20:10:08 +0000

    msm8916-common: Add prebuilt qti-telephony-common.jar

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,6 +139,9 @@ TARGET_LD_SHIM_LIBS := \
 # Vendor Interface Manifest
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 
+# Telephony
+TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
+
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn
