a396300c84d4455c4687e6fac05e00532b230b80
Kyle Harrison <khwebmail@gmail.com>
2019-01-29 19:47:31 +0000

    msm8916-common: Switch to legacymm LiveDisplay HAL

--- msm8916.mk
+++ msm8916.mk
@@ -199,7 +199,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-	vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@1.0-service-legacymm
 
 # Media
 PRODUCT_COPY_FILES += \
