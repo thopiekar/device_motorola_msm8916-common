3071c401eaeefe512103b433ab79c5103806f669
Wang Han <416810799@qq.com>
2019-10-18 17:55:22 +0100

    msm8916-common: Remove more deprecated display flags
    chil360: Also, enable TARGET_CONTINUOUS_SPLASH_ENABLED
    
    Change-Id: I7924f76ed864dea9cefbe2585be629bd2239c447

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,15 +109,10 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 TARGET_LEGACY_HW_DISK_ENCRYPTION := true
 
 # Display
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
-TARGET_USES_NEW_ION_API := true
-
-# Display Renderer
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-USE_OPENGL_RENDERER := true
 
 # Exclude serif fonts for saving system.img size.
 EXCLUDE_SERIF_FONTS := true
