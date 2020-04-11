42c8b8d77101314a47676bd82658ec7f6216bf00
althafvly <althafvly@gmail.com>
2020-04-11 21:29:11 +0530

    softap: sdk: Merge into DT
    https://github.com/LineageOS/android_system_qcom merge into device tree . because its only needed for motorola devices and lineage is not tracking it after pie

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -28,9 +28,45 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ASensorManager.cpp libqsap_shim.c
 LOCAL_SHARED_LIBRARIES := android.hardware.sensors@1.0 libqsap_sdk liblog
 LOCAL_STATIC_LIBRARIES := android.hardware.sensors@1.0-convert
-LOCAL_C_INCLUDES := $(TOP)/system/qcom/softap/sdk
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/qsap
 LOCAL_MODULE := libqsap_shim
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
+# QSAP_SDK
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := $(TOP)/hardware/libhardware_legacy/wifi $(TOP)/external/libnl/include $(TOP)/external/wpa_supplicant_8/wpa_supplicant/src/drivers
+LOCAL_MODULE:= libqsap_sdk
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+LOCAL_CFLAGS += -DSDK_VERSION=\"0.0.1.0\"
+LOCAL_USE_VNDK := true
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/qsap/qsap_api.h \
+                               $(LOCAL_PATH)/qsap/qsap.h
+
+LOCAL_CFLAGS += \
+    -Wall \
+    -Werror \
+    -Wno-unused-variable \
+    -Wno-unused-value \
+    -Wno-format \
+    -Wno-sometimes-uninitialized \
+    -Wno-enum-conversion \
+    -Wno-unused-parameter \
+    -Wno-implicit-function-declaration
+
+LOCAL_SRC_FILES := qsap/qsap_api.c \
+                   qsap/qsap.c
+
+LOCAL_PRELINK_MODULE := false
+LOCAL_SHARED_LIBRARIES := libnetutils libutils libbinder libcutils libhardware_legacy libnl liblog
+LOCAL_HEADER_LIBRARIES := libcutils_headers
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libqsap_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_HEADER_LIBRARY)
